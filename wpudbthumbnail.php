348f46ca63ad6e55491e570e7e901de0f7086744
Kevin <kevin@rocher-dev.com>
2017-02-12 23:59:03 +0100

    v 0.9
    * Compresse base64.
    * Add debug mode.

--- wpudbthumbnail.php
+++ wpudbthumbnail.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU DB Thumbnail
 Description: Store a small thumbnail in db
-Version: 0.8
+Version: 0.9
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -17,6 +17,9 @@ class wpudbthumbnail {
     private $image_size = 40;
     private $cache_in_file = false;
     private $post_types = 'any';
+    private $jpeg_prefix = '';
+    private $compress_base64 = true;
+    private $debug = false;
 
     public function __construct() {
         add_action('init', array(&$this, 'init'));
@@ -30,6 +33,7 @@ class wpudbthumbnail {
         $this->image_size = apply_filters('wpudbthumbnail_imagesize', $this->image_size);
         $this->post_types = apply_filters('wpudbthumbnail_posttypes', $this->post_types);
         $this->cache_in_file = apply_filters('wpudbthumbnail_cacheinfile', $this->cache_in_file);
+        $this->compress_base64 = apply_filters('wpudbthumbnail_compress_base64', $this->compress_base64);
 
         /* File cache settings */
         $upload_dir = wp_upload_dir();
@@ -41,11 +45,22 @@ class wpudbthumbnail {
         }
 
         /* Settings version */
-        $settings_key = md5($this->jpeg_quality . $this->image_size . serialize($this->post_types) . $this->cache_in_file);
+        $settings_key = md5($this->jpeg_quality . $this->image_size . serialize($this->post_types) . serialize($this->compress_base64) . $this->cache_in_file);
         $settings_version = get_option('wpudbthumbnail_settingsversion');
         if ($settings_version != $settings_key) {
             update_option('wpudbthumbnail_settingsversion', $settings_key);
             $this->clear_cache();
+            $this->update_jpeg_prefix();
+        }
+
+        $this->jpeg_prefix = get_option('wpudbthumbnail_jpegprefix');
+        if (!$this->jpeg_prefix) {
+            $this->update_jpeg_prefix();
+        }
+
+        if ($this->debug) {
+            $this->clear_cache();
+            $this->update_jpeg_prefix();
         }
     }
 
@@ -101,7 +116,16 @@ class wpudbthumbnail {
         }
 
         /* Save as base64 */
-        $base64 = $this->set_base64_thumb($post_thumbnail_id);
+        $base_image = get_attached_file($post_thumbnail_id);
+        $base64 = $this->generate_base64_thumb($base_image);
+
+        if ($this->compress_base64) {
+            $base64 = str_replace('data:image/jpeg;base64,/9j/', '#d#', $base64);
+            $base64 = str_replace($this->jpeg_prefix, '#j#', $base64);
+            if ($this->cache_in_file && function_exists('gzencode')) {
+                $base64 = gzencode($base64);
+            }
+        }
 
         if ($base64 !== false) {
             if ($this->cache_in_file) {
@@ -113,64 +137,13 @@ class wpudbthumbnail {
         }
     }
 
-    public function get_cachefilebase64($post_id) {
-        $this->cache_file = $this->cache_dir . 'post-' . $post_id . '.base64';
-        if (file_exists($this->cache_file)) {
-            return file_get_contents($this->cache_file);
-        }
-        return false;
-    }
+    public function generate_base64_thumb($base_image = false) {
 
-    public function set_cachefilebase64($post_id, $base64) {
-        $this->cache_file = $this->cache_dir . 'post-' . $post_id . '.base64';
-        file_put_contents($this->cache_file, $base64);
-    }
-
-    public function get_base64_thumb_value($post_id = false) {
-        if ($this->cache_in_file) {
-            return $this->get_cachefilebase64($post_id);
-        } else {
-            return get_post_meta($post_id, $this->meta_id, 1);
-        }
-    }
-
-    public function get_base64_thumb($post_id = false) {
-        global $post;
-        /* Retrieve current post */
-        if ($post_id === false) {
-            if (!is_object($post)) {
-                return false;
-            }
-            $post_id = $post->ID;
-        }
-
-        /* Retrieve thumb code if avalaible */
-        $thumbnailcode_id = get_post_meta($post_id, $this->meta_id . '_id', 1);
-        $thumbnailcode = $this->get_base64_thumb_value($post_id);
-
-        if ($this->cache_in_file) {
-            $thumbnailcode = $this->get_cachefilebase64($post_id);
-        } else {
-            $thumbnailcode = get_post_meta($post_id, $this->meta_id, 1);
-        }
-
-        /* No thumb code, but a thumbnail should exist, regenerate */
-        if (!$thumbnailcode && ($thumbnailcode_id > 0 || $thumbnailcode_id == '')) {
-            $this->save_post($post_id, true);
-            $thumbnailcode = $this->get_base64_thumb_value($post_id);
-        }
-
-        return $thumbnailcode;
-    }
-
-    public function set_base64_thumb($post_thumbnail_id = false) {
-
-        if (!$post_thumbnail_id) {
+        if (!$base_image) {
             return false;
         }
 
         /* Retrieve image */
-        $base_image = get_attached_file($post_thumbnail_id);
         $image = wp_get_image_editor($base_image);
         if (is_wp_error($image)) {
             return false;
@@ -184,7 +157,7 @@ class wpudbthumbnail {
             $type = 'jpg';
         }
         $upload_dir = wp_upload_dir();
-        $image->resize($this->image_size, $this->image_size, false);
+        $image->resize($this->image_size, $this->image_size, 1);
         if ($type == 'jpg') {
             $image->set_quality($this->jpeg_quality);
         }
@@ -222,6 +195,71 @@ class wpudbthumbnail {
         }
     }
 
+    public function get_cachefilebase64($post_id) {
+        $this->cache_file = $this->cache_dir . 'post-' . $post_id . '.base64';
+        if (file_exists($this->cache_file)) {
+            return file_get_contents($this->cache_file);
+        }
+        return false;
+    }
+
+    public function set_cachefilebase64($post_id, $base64) {
+        $this->cache_file = $this->cache_dir . 'post-' . $post_id . '.base64';
+        file_put_contents($this->cache_file, $base64);
+    }
+
+    public function get_base64_thumb_value($post_id = false) {
+        if ($this->cache_in_file) {
+            $base64 = $this->get_cachefilebase64($post_id);
+        } else {
+            $base64 = get_post_meta($post_id, $this->meta_id, 1);
+        }
+
+        if ($this->compress_base64) {
+            if ($this->cache_in_file && function_exists('gzencode')) {
+                $base64 = gzdecode($base64);
+            }
+            $base64 = str_replace('#d#', 'data:image/jpeg;base64,/9j/', $base64);
+            $base64 = str_replace('#j#', $this->jpeg_prefix, $base64);
+        }
+
+        return $base64;
+    }
+
+    public function get_base64_thumb($post_id = false) {
+        global $post;
+        /* Retrieve current post */
+        if ($post_id === false) {
+            if (!is_object($post)) {
+                return false;
+            }
+            $post_id = $post->ID;
+        }
+
+        /* Retrieve thumb code if available */
+        $thumbnailcode_id = get_post_meta($post_id, $this->meta_id . '_id', 1);
+        $thumbnailcode = $this->get_base64_thumb_value($post_id);
+
+        /* No thumb code, but a thumbnail should exist, regenerate */
+        if (!$thumbnailcode && ($thumbnailcode_id > 0 || $thumbnailcode_id == '')) {
+            $this->save_post($post_id, true);
+            $thumbnailcode = $this->get_base64_thumb_value($post_id);
+        }
+
+        return $thumbnailcode;
+    }
+
+    /* ----------------------------------------------------------
+      Prefix
+    ---------------------------------------------------------- */
+
+    public function update_jpeg_prefix() {
+        /* Extract common part in base64 strings */
+        $datab1 = explode('/', $this->generate_base64_thumb(dirname(__FILE__) . '/images/test.jpg'));
+        $this->jpeg_prefix = $datab1[4];
+        update_option('wpudbthumbnail_jpegprefix', $prefix);
+    }
+
     /* ----------------------------------------------------------
       Clear
     ---------------------------------------------------------- */
@@ -247,6 +285,9 @@ class wpudbthumbnail {
     ---------------------------------------------------------- */
 
     public function uninstall() {
+
+        delete_option('wpudbthumbnail_jpegprefix');
+
         // Clear cache
         $this->clear_cache();
 
